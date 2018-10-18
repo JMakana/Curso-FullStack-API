3688e81cea2c8ccb1b6cdfe1209b279b108ed15a
mcchaves <eleceya.2@gmail.com>
2018-10-18 19:56:52 +0200

    convirtiendo el create user en async

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -34,4 +34,21 @@ const create = (req, res) => {
     })
 }
 
+const createAsync = async (req, res) => {
+  // recogemos los datos del body que nos interesen:
+  const { email, password } = req.body
+  const user = new User({ email, password })
+  try {
+    const usuario = await user.save()
+    const token = await user.generateAuthToken()
+    res
+      .header('x-auth', token)
+      .status(201)
+      .send(usuario)
+  } catch (err) {
+    res.status(400).send({ error: err })
+  }
+}
+
 module.exports = { create }
+module.exports = { createAsync }
